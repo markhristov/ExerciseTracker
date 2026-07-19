903cc8ce1d16979adfa01bbe43e08a90a8c9c3e4
markhristov <marki.v.hristov@gmail.com>
2026-07-19 16:00:51 +0300

    Improve camera not working screen

--- app/src/main/java/com/example/exercisetracker/ui/ExerciseScreen.kt
+++ app/src/main/java/com/example/exercisetracker/ui/ExerciseScreen.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.ImageProxy
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,16 +22,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.example.exercisetracker.PoseUiState
 import com.example.exercisetracker.camera.CameraManager
 import com.example.exercisetracker.ui.theme.ExerciseTrackerTheme
-
 @Composable
 fun ExerciseScreen(
     uiState: PoseUiState,
@@ -136,10 +137,18 @@ fun CameraPreview(
         }
     }
 
-    AndroidView(
-        factory = { previewView },
-        modifier = modifier
-    )
+    if (isActive) {
+        AndroidView(
+            factory = { previewView },
+            modifier = modifier
+        )
+    } else {
+        Box(
+            modifier = modifier
+                .background(Color.Gray)
+        )
+    }
+
 }
 
 @Preview
